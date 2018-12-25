41951e2ef052abbdec61b558dfc2e0d2dad92593
ZubairAzimMiazi <39708656+ZubairAzimMiazi@users.noreply.github.com>
2018-12-25 20:28:27 +0600

    SelectionSort.h

--- SelectionSort.h
+++ SelectionSort.h
@@ -1,1 +1,12 @@
-
+#ifndef SELECTIONSORT_H_INCLUDED
+#define SELECTIONSORT_H_INCLUDED
+#include <vector>
+using namespace std;
+class SelectionSort
+{
+public:
+	SelectionSort();
+	~SelectionSort();
+	void SelectionSorting(int array[], int array_size);
+};
+#endif // !SELECTIONSORT_H_INCLUDED
