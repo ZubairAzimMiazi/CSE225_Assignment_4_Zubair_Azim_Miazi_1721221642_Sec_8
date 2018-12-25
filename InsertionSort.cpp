539a6931ccef7cefc074bdeb35603f6ec28b894e
ZubairAzimMiazi <39708656+ZubairAzimMiazi@users.noreply.github.com>
2018-12-25 20:25:34 +0600

    InsertionSort.cpp

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,1 +1,36 @@
+#include "InsertionSort.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+InsertionSort::InsertionSort()
+{
 
+}
+InsertionSort::~InsertionSort()
+{
+
+}
+void InsertionSort::InsertionSorting(int array[], int array_size)
+{
+
+	if (array_size > 1)
+	{
+		int size = array_size;
+		for (int i = 1; i < size; ++i)
+		{
+			int j = i - 1;
+			int key = array[i];
+			while (j >= 0 && array[j] > key)
+			{
+				array[j + 1] = array[j];
+				--j;
+			}
+			array[j + 1] = key;
+			for (int k = 0; k < array_size; ++k)
+			{
+				cout << array[k] << " ";
+			}
+			cout << endl;
+		}
+	}
+}
