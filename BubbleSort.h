5548eaf3b0fccde24f6a5a2ae79d45a2f79513fa
ZubairAzimMiazi <39708656+ZubairAzimMiazi@users.noreply.github.com>
2018-12-25 20:18:29 +0600

    BubbleSort.h
    This is the header file of bubble sorting.

---
+++ BubbleSort.h
@@ -0,0 +1,13 @@
+#ifndef BUBBLESORT_H_INCLUDED
+#define BUBBLESORT_H_INCLUDED
+#include<vector>
+using namespace std;
+class BubbleSort
+{
+public:
+	BubbleSort();
+	~BubbleSort();
+	void BubbleSorting(int Array[] , int count);
+
+};
+#endif // !BUBBLESORT_H_INCLUDED
