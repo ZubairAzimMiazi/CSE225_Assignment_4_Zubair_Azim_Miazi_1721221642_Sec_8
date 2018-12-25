49a7713423a0c7e40ea12882e2d5e706b244bd54
ZubairAzimMiazi <39708656+ZubairAzimMiazi@users.noreply.github.com>
2018-12-25 20:26:45 +0600

    InsertionSort.h

--- InsertionSort.h
+++ InsertionSort.h
@@ -1,1 +1,13 @@
-
+#ifndef INSERTION_H_INCLUDED
+#define INSERTION_H_INCLUDED
+#include <vector>
+using namespace std;
+class InsertionSort
+{
+public:
+	InsertionSort();
+	~InsertionSort();
+	void InsertionSorting(int array[], int array_size);
+	void InsertionSorting(vector<int>, int size);
+};
+#endif // !INSERTION_H_INCLUDED
